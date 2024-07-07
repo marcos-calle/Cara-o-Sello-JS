3e71ea63a0757f9ea5ad428dc96f8b0ea317f939
mark <mark16calle@gmail.com>
2024-07-06 21:04:59 -0500

    caraSello v2.1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const monedaDiv = document.getElementById("moneda");
 const play = document.getElementById("play");
 const spanHeads = document.getElementById("countHeads");
 const spanTails = document.getElementById("countTails");
-let estilo = "";
 let countHeads = 0;
 let countTails = 0;
 const moneda = {
@@ -22,11 +21,7 @@ play.addEventListener("click", async () => {
   monedaDiv.classList.add("moneda-animation");
   const valor = await lanzarMoneda();
   monedaDiv.classList.remove("moneda-animation");
-
-  // estilo = valor === "sello" && "sello";
-
   if (valor === "sello") {
-    estilo = valor;
     monedaDiv.classList.add(valor);
     countTails++;
     spanTails.innerText = countTails;
